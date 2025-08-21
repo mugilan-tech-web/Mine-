cd5f6fe20e2198ef3a654e1e798d66b7b9501ee2
mkarmugilan4990-lang <mkarmugilan4990@gmail.com>
2025-08-21 15:12:51 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,10 @@
+
+public class Arithmetic {
+    
+    public static void main(String[] args) {
+        
+        for(int i=0;i<=1;i++){
+            System.out.println("Hello, World!");
+    }
+    }
+}
